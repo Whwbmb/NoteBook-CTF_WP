d839656d791426b3be89e04f49e5b2ed3a707599
Whwbmb <147323262+Whwbmb@users.noreply.github.com>
2025-07-23 21:28:04 +0800

    update:web

--- WEB/脚本/反序列化.php
+++ WEB/脚本/反序列化.php
@@ -10,14 +10,34 @@
 //         }  
 //     }  
 // }  
-class Flag{  
-    public $file;  
-    public function __construct(){  
-        $this->file = "flag.php";  
-    }
+
+
+// class Flag{  
+//     public $file;  
+//     public function __construct(){  
+//         $this->file = "flag.php";  
+//     }
+// }
+// $file=new Flag();
+// $flag=serialize($file);
+// $flag1=urlencode($flag);
+// echo $flag1;
+// ?>  
+
+
+<?php  
+$b1="MAUXXQC";
+$b2="MMHUWUV";
+// if($b1!=$b2&&md5($b1)==md5($b2)){
+if($b1!=$b2&&md5($b1)===md5($b2)){
+    
+    echo "true\n";
+    echo "flag{".md5($b1)."}\n";
+    echo "flag{".md5($b2)."}";
+
+}else{
+    echo "false\n";
+    echo "flag{".md5($b1)."}\n";
+    echo "flag{".md5($b2)."}";
 }
-$file=new Flag();
-$flag=serialize($file);
-$flag1=urlencode($flag);
-echo $flag1;
 ?>  
